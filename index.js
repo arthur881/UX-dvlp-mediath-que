2a93086fc08db1630fc9645aba6209f9090ee9d0
RENAUD Brevin <renaudbrevin@gmail.com>
2023-02-23 18:03:53 +0100

    :sparkles: Add forms part

--- index.js
+++ index.js
@@ -11,3 +11,37 @@ burgerBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {  
     burgerMenu.classList.remove('active');
 });
+
+
+var allInputs = document.querySelectorAll('input');
+
+// Valide input
+
+allInputs.forEach(input => {
+    input.addEventListener('blur', () => {
+        if (input.value == "") {
+            if(input.classList.contains('valid')) {
+                input.classList.remove('valid');
+                input.classList.add('error');
+            }
+        } else {
+            if(input.classList.contains('error')) {
+                input.classList.remove('error');
+            }
+            input.classList.add('valid');
+        }
+    })
+})
+
+var submitButton = document.querySelector('form button');
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    allInputs.forEach(input => {
+        if (input.value == "") {
+            input.classList.add('error');
+        } else {
+            input.classList.add('valid');
+        }
+    })
+})
