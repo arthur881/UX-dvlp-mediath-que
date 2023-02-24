6e0e964da8f93e823fc9b4b2338c3676310b0f28
Hugo Senée <113121085+Hugosenee@users.noreply.github.com>
2023-02-24 10:17:52 +0100

    :sparkles: buttons animation, scroll reveal and btn flechedone

--- index.js
+++ index.js
@@ -45,3 +45,39 @@ submitButton.addEventListener('click', (e) => {
         }
     })
 })
+
+// apparaitre div scroll
+
+const timing = .65
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: timing
+}
+
+const HandleIntersect = function (entries , observer) {
+    entries.forEach((entry) => {
+        if(entry.intersectionRatio > timing){
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        }
+    })
+}
+
+const observer = new IntersectionObserver(HandleIntersect, options);
+document.querySelectorAll('.reveal').forEach(function (reveal) {
+    observer.observe(reveal)
+});
+
+// button top
+
+let btn = document.getElementById("flechehaut");
+let section = document.getElementById("landing");
+
+window.addEventListener("scroll", function() {
+    if (window.scrollY >= section.offsetTop) {
+        btn.classList.add("active");
+    } else {
+        btn.classList.remove("active");
+    }
+});
